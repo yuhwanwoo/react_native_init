50b2057e96912ad84a0d3a78933068594093ed72
yuhwanwoo <6514687@naver.com>
2021-01-01 23:08:49 +0900

    error

---
+++ fucking-weather/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends Component{
+  state={
+    isLoaded:false
+  }
+  render(){
+    const {isLoaded}=this.state;
+    return (
+    <View style={styles.container}>
+      {isLoaded ? null : (
+      <View style={styles.loading}>
+        <Text style={styles.loadingText}>Getting the fucking weather</Text>
+        </View>
+      )}
+    </View>
+  );
+      }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  loading:{
+    flex:1,
+    backgroundColor:"#FDF6AA",
+    justifyContent:"flex-end",
+    paddingLeft:25,
+  },
+  loadingText:{
+    fontSize:38,
+    marginBottom:100,
+  }
+});
---
+++ fucking-weather/weather.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, Veiw} from 'react-native';
+import {LinearGradient} from 'expo';
+
+export default class Weather extends Component{
+    
+}
