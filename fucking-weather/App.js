3f72ad5bd5abab42c12463a575287c5c46e49181
yuhwanwoo <6514687@naver.com>
2021-01-02 17:01:50 +0900

    weather

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from "./Weather"
 
 export default class App extends Component{
   state={
-    isLoaded:false
+    isLoaded:false,
+    error:null,
+  }
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(position=>{
+      this.setState({
+        isLoaded:true
+      });
+    },
+    error=>{
+      this.setState({
+        error:error
+      })
+    }
+    );
   }
   render(){
-    const {isLoaded}=this.state;
+    const {isLoaded,error}=this.state;
     return (
     <View style={styles.container}>
-      {isLoaded ? null : (
+      <StatusBar hidden={true} />
+      {isLoaded ? (
+        <Weather />
+      ) : (
       <View style={styles.loading}>
         <Text style={styles.loadingText}>Getting the fucking weather</Text>
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
         </View>
       )}
     </View>
@@ -25,6 +44,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+    marginBottom:40,
+  },
   loading:{
     flex:1,
     backgroundColor:"#FDF6AA",
@@ -33,6 +57,6 @@ const styles = StyleSheet.create({
   },
   loadingText:{
     fontSize:38,
-    marginBottom:100,
+    marginBottom:24,
   }
 });
--- fucking-weather/weather.js
+++ fucking-weather/weather.js
@@ -1,7 +1,63 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, Veiw} from 'react-native';
-import {LinearGradient} from 'expo';
+import React, {Component, version} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component{
-    
+    render(){
+        return (
+        <LinearGradient
+            colors={["#00C6FB","#005BEA"]}
+            style={styles.container}
+        >
+            <View style={styles.upper}>
+                <Ionicons color="white" size={144} name="ios-rainy"/>
+                <Text style={styles.temp}>35*</Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>Rainging like a MF</Text>
+                <Text style={styles.subtitle}>For more info look outsie</Text>
+            </View>
+        </LinearGradient>
+        );
+    }
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    upper:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:"transparent",
+    },
+    temp:{
+        fontSize:48,
+        backgroundColor:"transparent",
+        color:"white",
+        marginTop:10,
+    },
+    lower:{
+        flex:1,
+        alignItems:"flex-start",
+        justifyContent:'flex-end',
+        paddingLeft:25,
+    },
+    title:{
+        fontSize:38,
+        backgroundColor:"transparent",
+        color:"white",
+        marginBottom:10,
+        fontWeight:"300"
+    },
+    subtitle:{
+        fontSize:24,
+        backgroundColor:"transparent",
+        color:"white",
+        marginBottom:24,
+
+    },
+});
